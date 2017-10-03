1b325639c5cf3725653d6fc4f36c0404bf3b5ed0
kiranamr <31201784+kiranamr@users.noreply.github.com>
2017-10-03 16:51:52 +0530

    port number changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-var port=process.env.PORT || 3000;
+var port=process.env.PORT || 80;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var path=require('path');
